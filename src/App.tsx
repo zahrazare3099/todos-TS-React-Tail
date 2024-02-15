139b91ba3610ffb0d1af2a84e3fe98343decc720
ZahraZare <zahrazare3099@gmail.com>
2024-02-15 14:22:29 +0330

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,67 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Table from "./components/Table";
 import ReminderServices from "./service/ReminderServices";
 import { Reminder } from "./models/reminder";
+import useLocalStorage from "./components/useLocalStorage";
+import { v4 as uuidv4 } from "uuid";
 
 export interface ReminderList {
   items: Array<Reminder>;
   onDelete: (id: number) => void;
   onEdit: (id: number) => void;
 }
+export type Note = {
+  id: string;
+} & Notedata;
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
+export type RawNote = {
+  id: string;
+} & RawNotedata;
+
+export type RawNotedata = {
+  title: string;
+  tagIds: string[];
+};
+
+export type Notedata = {
+  title: string;
+  tags: Tag[];
+};
+
 function App() {
   const [data, setData] = useState<Reminder[]>([]);
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("Tags", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes?.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+  console.log("notesWithTags", notesWithTags);
+
+  function onCreateNote({ tags, ...data }: Notedata) {
+    setNotes((preNotes) => {
+      return [
+        ...preNotes,
+        { ...data, id: uuidv4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
+
+  function addTag(tag: Tag) {
+    setTags((prev) => (prev ? [...prev, tag] : [tag]));
+  }
 
   useEffect(() => {
     const loadReminders = async () => {
@@ -20,16 +70,17 @@ function App() {
     };
     loadReminders();
   }, []);
+
   const handleDelete = (id: number) => {
     setData(data.filter((item) => item.id !== id));
   };
 
-  const addReminder = async (title: string) => {
-    // const newReminder:Reminder = await ReminderServices.addReminders(title);
-    // console.log(newReminder);
+  // const addReminder = async (title: string) => {
+  //   // const newReminder:Reminder = await ReminderServices.addReminders(title);
+  //   // console.log(newReminder);
+  //   setData([{ title, id: Date.now() }, ...data]);
+  // };
 
-    setData([{ title, id: Date.now() }, ...data]);
-  };
   return (
     <div className="px-3 pb-6 bg-slate-900 h-full text-slate-100">
       <div className="p-3 border-b flex gap-x-2 items-center">
@@ -41,7 +92,12 @@ function App() {
         <h1 className="font-bold">Reminder App</h1>
       </div>
       <div>
-        <Form onAdd={addReminder} />
+        <Form
+          // onAdd={addReminder}
+          onCreateNote={onCreateNote}
+          addTag={addTag}
+          availableTags={tags}
+        />
       </div>
       <div className="min-w-xl px-4">
         <Table items={data} onDelete={handleDelete} />
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,75 @@
 import React from "react";
+import { Notedata, Tag } from "../App";
+import CreatableSelect from "react-select/creatable";
+import { v4 as uuidv4 } from "uuid";
+// interface NewReminderListProps {
+//   onAdd: (title: string) => void;
+// }
+type NoteFormProps = {
+  onCreateNote: (data: Notedata) => void;
+  addTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
 
-interface NewReminderListProps {
-  onAdd: (title: string) => void;
-}
-
-export default function Form({ onAdd }: NewReminderListProps): JSX.Element {
+export default function Form(
+  // { onAdd }: NewReminderListProps,
+  { onCreateNote, addTag, availableTags }: NoteFormProps
+): JSX.Element {
   const [title, setTitle] = React.useState("");
+  const [selectTags, setSelectTags] = React.useState<Tag[]>([]);
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (title == "") return;
-    onAdd(title);
+    // onAdd(title);
+    onCreateNote({ title, tags: selectTags });
     setTitle("");
   };
+
   return (
-    <div className="p-4">
-      <form className="flex gap-x-2 items-center" onSubmit={handleSubmit}>
+    <div className="p-4 border my-3 rounded-xl gap-3 flex flex-col">
+      <h1 className="font-bold text-center">+Reminder</h1>
+      <form
+        className="flex flex-wrap gap-3 w-full justify-center "
+        onSubmit={handleSubmit}
+      >
         <input
           name="title"
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           placeholder="title"
-          className="bg-slate-800 rounded-lg text-slate-200 px-2 py-1 w-1/2"
+          className="bg-slate-800 rounded-lg text-slate-200 px-2 py-1  w-72 md:w-56 lg:w-72"
         />
-        <button type="submit" className="bg-green-800 w-24 py-1 rounded-lg">
+        <CreatableSelect
+          className="bg-transparent rounded-lg outline-none text-slate-200 w-72 md:w-52 lg:w-72"
+          classNamePrefix="classNamePrefix bg-transparent text-slate-200 outline-none"
+          placeholder="selecte options"
+          onCreateOption={(label) => {
+            const newTag = { id: uuidv4(), label };
+            addTag(newTag);
+            setSelectTags((prev) => [...prev, newTag]);
+          }}
+          isMulti
+          value={selectTags.map((tag) => {
+            return { label: tag.label, value: tag.id };
+          })}
+          onChange={(tags) => {
+            setSelectTags(
+              tags.map((tag) => {
+                return { label: tag.label, id: tag.value };
+              })
+            );
+          }}
+          options={availableTags?.map((tag) => {
+            return { label: tag.label, value: tag.id };
+          })}
+        />
+
+        <button
+          type="submit"
+          className="bg-green-800 py-1 md:p-0 rounded-lg  w-72 md:w-48"
+        >
           Add
         </button>
       </form>
